bb3484e8f9fa3c02d2bea3e5e4e4939ee867d09a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-09-02 19:36:07 +0600

    get allcars data in db

--- src/Pages/AllCars/AllCars.jsx
+++ src/Pages/AllCars/AllCars.jsx
@@ -1,9 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useInstance from '../../Hooks/useInstance';
 
 const AllCars = () => {
+    const instance = useInstance();
+    //? TranStack Query using get AllCars Data in db;
+    const {data:allcars=[]} = useQuery({
+        queryKey:['allCars'],
+        queryFn:async()=>{
+            const res = await instance.get('/allCars')
+            return res.data
+        }
+    })
     return (
         <div>
-            <h1>Hi All Cars Pages here!</h1>
+            <h1>Hi All Cars Pages here!{allcars.length}</h1>
         </div>
     );
 };
